9f4c18adff886ca4ba052de2ca3adb4b0b98c3cb
Aleksandr <116256628+LavrinovichA@users.noreply.github.com>
2024-03-25 11:08:01 +0300

    Update imports.py
    Обновлен список библиотек

--- imports.py
+++ imports.py
@@ -1,8 +1,11 @@
-import telebot
-import time
-import datetime
-from datetime import datetime
-from telebot import types
 import re
-import threading
+import time
 import json
+import threading
+from datetime import datetime
+import telebot
+from telebot import types
+import logging
+from PIL import Image
+import pytesseract
+from io import BytesIO
