bf161071cd99d0d4f1916836674117ffc57dc3ab
Aleksandr <116256628+LavrinovichA@users.noreply.github.com>
2024-04-11 16:01:49 +0300

    Add files via upload
    Обновленный файл

---
+++ multibot/imports.py
@@ -0,0 +1,12 @@
+import re
+import time
+import json
+import threading
+from datetime import datetime
+import telebot
+from telebot import types
+import logging
+from PIL import Image
+import pytesseract
+from io import BytesIO
+import socket
